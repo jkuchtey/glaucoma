9cf366455552cf1be384b37921c7b458d1966b70
Ethan Nissenson <enissens@ssdl137-15.public.oberlin.edu>
2024-04-28 16:15:55 -0400

    more shit code

--- glaucoma_model.py
+++ glaucoma_model.py
@@ -1,40 +1,50 @@
-import tensorflow as tf
+#import tensorflow as tf
 import matplotlib as plt
 import pandas as pd
 import numpy as np
 import sys
 import os
+import shutil
 
 directory = sys.argv[1]
 
 
 if directory == 'G1020':
+    labeldata = pd.read_csv(f'G1020/G1020.csv')
+    for filename in os.listdir('G1020/Images'):
+        #print(image)
+        f = os.path.join('G1020/Images', filename)
+        if f.endswith(".json"):
+            os.remove(f)
+    for image in os.listdir('G1020/Images'):
+        imageindex =  os.listdir('G1020/Images').index(image)+1    
+        label = str(labeldata.iloc[imageindex]['binaryLabels'])
+        if not os.path.exists(f"G1020/Class/{label}"): 
+            os.makedirs(f"G1020/Class/{label}") 
+        # shutil.move(f"G1020/Images/{image}", f"G1020/Classifieds/{label}/{image}")
 
-    labeldata = pd.read_csv('G1020.csv')
-    for label in labeldata:
-        for image in os.listdir('GLAUCOMA/G1020/Images'):
-            if image.endswith(".jpg"):
+    # tf.keras.preprocessing.image_dataset_from_directory(
+    #     directory,
+    #     labels='inferred',
+    #     label_mode="binary",
+    #     class_names=None,
+    #     color_mode='rgb',
+    #     batch_size=32,
+    #     image_size=(2656, 2048),
+    #     shuffle=True,
+    #     seed=None,
+    #     validation_split=None,
+    #     subset=None,
+    #     interpolation='bilinear',
+    #     follow_links=False,
+    #     crop_to_aspect_ratio=False,
+    #     pad_to_aspect_ratio=False,
+    #     data_format=None,
+    #     verbose=True
+    # )
 
-
-                tf.keras.preprocessing.image_dataset_from_directory(
-                    directory,
-                    labels='inferred',
-                    label_mode="binary",
-                    class_names=None,
-                    color_mode='rgb',
-                    batch_size=32,
-                    image_size=(2656, 2048),
-                    shuffle=True,
-                    seed=None,
-                    validation_split=None,
-                    subset=None,
-                    interpolation='bilinear',
-                    follow_links=False,
-                    crop_to_aspect_ratio=False,
-                    pad_to_aspect_ratio=False,
-                    data_format=None,
-                    verbose=True
-                )
+  # for image in os.listdir('G1020/Classifieds/0'):
+    #     shutil.move(f"G1020/Classifieds/0/{image}", f"G1020/Images/{image}")
 
 # def create_cnn(training_X, training_y):
 #     # get the shape of each image so the the first layer knows what inputs it will receive
@@ -60,4 +70,4 @@ if directory == 'G1020':
 #     # layers = [conv1, pool1, conv2, pool2, conv3, flat, dense, out]    
 #     network = tf.keras.models.Sequential(layers)
 
-#     return network
+#     return networkG1020/Images
