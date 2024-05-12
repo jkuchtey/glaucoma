7243a3ac0df3c11da5fa7eb8de3b192c219a5a3e
jason <jkuchtey@oberlin.edu>
2024-05-12 19:06:33 -0400

    first successful run

--- origa_processing.py
+++ origa_processing.py
@@ -30,64 +30,81 @@ lr = 0.01
 #     subset = "both", 
 # )
 
-ds = tf.keras.utils.image_dataset_from_directory(directory)
-
-data_iterator = ds.as_numpy_iterator()
-batch = data_iterator.next()
-fig, ax = plt.subplots(ncols=4, figsize=(20,20))
-for idx, img in enumerate(batch[0][:4]):
-    ax[idx].imshow(img.astype(int))
-    ax[idx].title.set_text(batch[1][idx])
-
-plt.show()
-
-
-def create_network(hct, oct):
-    hidden_layer = tf.keras.layers.Dense(hct, activation='sigmoid') 
-    output_layer = tf.keras.layers.Dense(oct)
-
-    all_layers = [hidden_layer, output_layer]
-    network = tf.keras.models.Sequential(all_layers)
-
-    return network
-
-def train_network(network, training_X, training_y, oct, lr):
-    # create the algorithm that learns the weight of the network 
-    optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
-    # create the loss function function that tells optimizer how much error it has in its predictions
-    if oct == 1:
-        loss_function = tf.keras.losses.MeanSquaredError()
-        network.compile(optimizer=optimizer, loss=loss_function, metrics=["mse"])
-
-    else:
-        loss_function = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-        network.compile(optimizer=optimizer, loss=loss_function, metrics=["accuracy"])
-
-    # prepare the network for training
-
-    # create a logger to save the training details to file
-    csv_fname  = "test.csv"
-    csv_logger = tf.keras.callbacks.CSVLogger(csv_fname)
-
-    # train the network for 250 epochs (setting aside 20% of the training data as validation data)
-    network.fit(training_X, training_y, validation_split=0.2, epochs=250, callbacks=[csv_logger])
+data = tf.keras.utils.image_dataset_from_directory(
+    directory, 
+    shuffle=True, 
+    seed=seed, 
+    validation_split=split, 
+    subset="both", 
+    labels="inferred", 
+    label_mode="binary", 
+    class_names=["0", "1"], 
+    batch_size=batch_size)
 
 
-def sep_x_y(ds):
-    set_X = []
-    set_y = []
 
-    for x, y in ds:
-        if not set_X:
-            set_X = [x]
-        else:
-            set_X = set_X.append(x)
-        if not set_y:
-            set_y = [y]
-        else:
-            set_y = set_y.append(y)
 
-    return set_X, set_y
+
+#Scale images
+tt = []
+for i in [0, 1]:
+    ds = data[i].map(lambda x,y: (x/255, y))
+    tt.append(ds)
+    data[i].as_numpy_iterator().next()
+
+training = tt[0]
+testing = tt[1]
+
+def show_imgs(data):
+    data_iterator = data.as_numpy_iterator()
+    batch = data_iterator.next()
+    fig, ax = plt.subplots(ncols=5, figsize=(20,20))
+
+
+    #show images
+    for idx, img in enumerate(batch[0][:5]):
+        ax[idx].imshow(img)
+        ax[idx].title.set_text(batch[1][idx])
+    batch = data_iterator.next()
+
+    plt.show()
+#Seperate labels and attributes
+
+print(data[0].class_names)
+
+
+def create_model(num_classes):
+    model = tf.keras.Sequential([
+    tf.keras.layers.Rescaling(1./255),
+    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(num_classes)
+    ])
+
+    model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'])
+
+    return model
+
+
+
+def train_model(model, training, testing, epochs):
+    model.fit(
+    training,
+    validation_data=testing,
+    epochs=epochs
+)
+
+model = create_model(2)
+train_model(model, data[0], data[1], 50)
 
 # training_X, training_y = sep_x_y(ds_training)
 # testing_X, testing_y = sep_x_y(ds_validation)
@@ -95,15 +112,6 @@ def sep_x_y(ds):
 # train_x_batches = np.concatenate([x for x, y in ds_training], axis=0)
 # train_y_batches = np.concatenate([y for x, y in ds_training], axis=0)
 
-print(ds)
-
-
-
-# print(train_x_batches)
-
-# x_train = ds_training.map(lambda i: i['image'])
-# y_train = ds_training.map(lambda l: l['label'])
-# x_test = ds_validation.map(lambda x: x['image'])
 # y_test = ds_validation.map(lambda y: y['label'])
 
 
@@ -113,3 +121,4 @@ print(ds)
 
 
 
+training
